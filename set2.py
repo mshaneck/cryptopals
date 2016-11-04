3eb5f5ecc4b6ef8fb9e996d6b248058adc0ff9ec
Mark Shaneck <mshaneck@liberty.edu>
2016-11-04 06:07:55 -0400

    Solved Set 2, Challenge 9

--- set2.py
+++ set2.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
+from Crypto.Cipher import AES
 
 def pkcs7Padding(data, blocksize):
     # First determine how much is needed
     bytesNeeded = blocksize-len(data)%blocksize
-    print bytesNeeded
+    return data + chr(bytesNeeded)*bytesNeeded
 
-def set2challenge1():
+def set2challenges9():
     print pkcs7Padding("YELLOW SUBMARINE", 20)
     print pkcs7Padding("Testing Testing 123", 10)
     print pkcs7Padding("This is a test", 14)
+    print pkcs7Padding("Test2", 75)
 
-set2challenge1()
+#set2challenge()
+
+def aes-128-ecb(input, key, mode):
+    cipher = AES.new(key, AES.MODE_ECB)
+    y = ""
+    if (mode == "decrypt"):
+        return cipher.decrypt(input)
+    else:
+        return cipher.encrypt(input)
+
+def aes-128-cbc(input, key, iv):
+
+
+
