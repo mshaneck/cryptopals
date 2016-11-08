0ce155d2378bec36639a6986f2d44213fecd1cb2
Mark Shaneck <mshaneck@liberty.edu>
2016-11-08 13:14:38 -0500

    Completed Set 2 Challenge 14

--- set2.py
+++ set2.py
@@ -7,6 +7,13 @@ import string
 
 ENCRYPT="e"
 DECRYPT="d"
+USE_PREFIX=True
+
+
+def getRandomString():
+    length = random.randint(1,200)
+    randStr = "{0:0{1}x}".format(random.getrandbits(length*8), length*2).decode('hex')
+    return randStr
 
 def splitIntoBlocks(data, blockSize):
     o = []
@@ -123,11 +130,20 @@ def set2challenge11():
 #set2challenge11()
 
 consistent_key = getRandomAESKey()
+random_string = getRandomString()
+# Just for debugging purposes
+#print len(random_string)
+#print len(random_string) % 16
+
 def aes_oracle(plaintext):
     #print plaintext
     #print len(plaintext)
+    prefix = ""
+    if (USE_PREFIX):
+        prefix=random_string
     secretString = base64.b64decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
-    paddedPlaintext = pkcs7Padding(plaintext + secretString, AES.block_size)
+    paddedPlaintext = pkcs7Padding(prefix+plaintext + secretString, AES.block_size)
+    print paddedPlaintext
     #print len(secretString)
     #print len(paddedPlaintext)
     ciphertext = aes_128_ecb(paddedPlaintext, consistent_key, ENCRYPT)
@@ -135,6 +151,7 @@ def aes_oracle(plaintext):
     return ciphertext
 
 def set2challenge12():
+    USE_PREFIX=False
     A="A"
     # Discover block size by feeding ECB oracle increasing size inputs to discover the block size
     ctxtlen=len(aes_oracle(A))
@@ -237,5 +254,93 @@ def set2challenge13():
 
 
 
-set2challenge13()
+#set2challenge13()
 
+def set2challenge14():
+    USE_PREFIX=True
+    A="A"
+    # Discover block size by feeding ECB oracle increasing size inputs to discover the block size
+    ctxtlen=len(aes_oracle(""))
+    blockSize=-1
+    for i in range(64):
+        ctxt = aes_oracle(A*i)
+        if len(ctxt) > ctxtlen:
+            blockSize = len(ctxt)-ctxtlen
+            #print blockSize
+            break
+    #print blockSize
+
+    # detect that it is using ECB mode
+    # Need to use 3 blocks of A since we don't know how long the prefix string is
+    if (isECB(aes_oracle(A*(3*blockSize)))):
+        usingECB=True   
+        #print "Using ECB"
+    else:
+        print "Not using ECB"
+        return
+    
+    blocks = splitIntoBlocks(aes_oracle(A*(3*blockSize)).encode('hex'), blockSize*2)
+    #print blocks;
+    # Find the blocks that match our input
+    x=-1
+    y=-1
+    for i, block in enumerate(blocks):
+        if (blocks[i] == blocks[i+1]):
+            x=i
+            y=i+1
+            break
+    #print x, y
+
+    # Next find the offset
+    # by sending 2 blocks of As, followed by a third that ends in a Z
+    # repeatedly increase the starting A's in the third block until you get the two repeated ciphertext blocks
+    # that were used to detect ECB mode
+    # the moment that they are equal, that means that the Z is pushed out into the next block
+    # The number of leading A's in that third block is the offset
+    offset=-1
+    for i in range(blockSize):
+        testplaintext = A*2*blockSize + A*i + 'Z'
+        #print testplaintext
+        ciphertext = aes_oracle(testplaintext)
+        blocks=splitIntoBlocks(ciphertext.encode('hex'), blockSize*2)
+        if (blocks[x] == blocks[y]):
+            offset=i
+            break
+
+    print "Offset is ", offset
+
+    # Compute the number of unknown blocks
+    numberOfUnknownBlocks = len(aes_oracle(""))/blockSize - x
+
+    # Now we can proceed with the previous algorithm, just adding A*offset before our strings
+    # and adding x to the j block offset
+
+    crack={}
+
+    decryptedMessage = ""
+    # Decrypt each block, one at a time
+    # Added one to the numberof unknown blocks since if the offset is large, it doesn't decrypt the final block
+    for j in range(numberOfUnknownBlocks+1):
+        # j is the block I need to keep
+        # Decrypt the block iteratively, since you can 
+        for i in range(blockSize):
+            addToDecryptionDictionary(A*offset + A*(blockSize-i-1)+decryptedMessage, crack, j+x, blockSize)
+            block = getHexBlock(aes_oracle(A*offset + A*(blockSize-i-1)), j+x, blockSize)
+
+            # at the very end, after the last byte,
+            # the padding changes since the size of the message is changing 
+            # So you cannot break that final byte
+            # so if the block that you obtain is not in the dictionary
+            # then you are done and can stop
+            if block in crack:
+                if (crack[block] == '\x01'):
+                    # We are into the padding area. Maybe?
+                    break
+                decryptedMessage += crack[block]
+            else:
+                # If its not there, we are done
+                break
+
+    print decryptedMessage.rstrip('\n') # Rstrip since the plaintext already includes a newline
+
+set2challenge14()
