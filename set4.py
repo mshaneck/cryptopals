66dd4ea54684ef7fdc691611bb7c72c4fdbda380
Mark Shaneck <mshaneck@liberty.edu>
2016-12-27 20:33:10 -0500

    Completed Challenge 25

--- set4.py
+++ set4.py
@@ -3,7 +3,6 @@ from set1 import *
 from set2 import *
 from set3 import *
 
-
 def edit(ciphertext, key, nonce, offset, newtext):
     # It seems to me that I will essentially just decrypt the whole thing, replace the plaintext with newtext, then reencrypt...
     # I suppose ideally I would generate just the piece that I need, but it's proof of concept stuff, right?
@@ -21,19 +20,20 @@ def set4challenge25():
     recoveredPlaintext = cipher.decrypt(x)
     ctrCiphertext = aes_128_ctr(recoveredPlaintext, consistent_key, consistent_nonce)
 
-    print "Testing the edit function"
-    testpt = "Well hello there, pardner. How are you doing today?"
-    print "Plaintext: "
-    print testpt
-    cipher = aes_128_ctr(testpt, consistent_key, consistent_nonce)
-    newtext = "goodbye bub"
-    cipher = attackeredit(cipher, 5, newtext)
-    print aes_128_ctr(cipher, consistent_key, consistent_nonce)
+    #print "Testing the edit function"
+    #testpt = "Well hello there, pardner. How are you doing today?"
+    #print "Plaintext: "
+    #print testpt
+    #cipher = aes_128_ctr(testpt, consistent_key, consistent_nonce)
+    #newtext = "goodbye bub"
+    #cipher = attackeredit(cipher, 5, newtext)
+    #print aes_128_ctr(cipher, consistent_key, consistent_nonce)
 
-
-    # TODO:
     # use the attackeredit function to recover the plaintext
-
-
+    newtext = "\x00"*len(recoveredPlaintext)
+    newcipher = attackeredit(ctrCiphertext, 0, newtext)
+    # now xor the newcipher with the ctrCiphertext
+    originalPlaintext = hexxor(newcipher.encode("hex"), ctrCiphertext.encode("hex")).decode("hex")
+    print originalPlaintext
 
 set4challenge25()
